1bec74c0368690a1e49cb65ee1b2e421142463ea
yogesh <yogesh@gmail.com>
2022-07-05 13:26:17 +0530

    HI

---
+++ JENKINS/disable.groovy
@@ -0,0 +1,4 @@
+pipelineJob("Folder-A/BACKUP/MSR/DEV/New_Job1"){
+    using("Folder-A/BACKUP/MSR/DEV/New_Job1")
+    disabled(true)
+}
---
+++ JENKINS/f1.groovy
@@ -0,0 +1,21 @@
+
+folder('Folder-A') {
+    displayName('Folder-A')
+    description('Folder for Newjob')
+}
+folder('Folder-A/BACKUP') {
+    displayName('BACKUP')
+    description('Folder for Newjob')
+}
+folder('Folder-A/BACKUP/MSF') {
+    displayName('MSF')
+    description('Folder for Newjob')
+}
+folder('Folder-A/BACKUP/MSR') {
+    displayName('MSR')
+    description('Folder for Newjob')
+}
+folder('Folder-A/BACKUP/MSR/DEV') {
+    displayName('DEV')
+    description('Folder for Newjob')
+}
